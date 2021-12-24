7c3f21752f3a055cf814066968cf2e82b3b662a2
sonalikokalki <sonalikokalki@gmail.com>
2021-12-24 15:32:07 +0530

    initiall commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('minutes');
+const secsEl = document.getElementById('seconds');
+const newYear = "1 Jan 2022";
+
+function countdown(){
+    const newYearDate = new Date(newYear);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearDate-currentDate)/1000;
+    
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600)% 24;
+    const mins = Math.floor(totalSeconds / 36)% 60;
+    const secs = Math.floor(totalSeconds) % 60;
+    console.log(days , hours , mins ,secs);
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secsEl.innerHTML = secs;
+
+}
+
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : time;
+}
+countdown();
+
+setInterval(countdown , 1000);
